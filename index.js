c2d341ebb0d1d71f9210feede8cb146db8ef8279
Jeffrey Zaayman <jeffrey.zaayman.osv@fedex.com>
2023-05-30 14:43:44 +0200

    Complex input

--- index.js
+++ index.js
@@ -2,14 +2,34 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+
+    var config = {
+        path: '.',
+        build: true,
+        publish: true,
+        lint: true,
+        unittest: true,
+        release: true,
+        plan: true,
+        deploy: true,
+        environments: ['sandbox', 'runtime-prd'],
+        autoDeployToPrd: true,
+    }
+
+    let input = core.getInput('config');
+    let _input = JSON.parse(input);
+
+    let finalConfig = Object.assign(config, _input);
+
+    console.log(`finalConfig: ${JSON.stringify(finalConfig)}`);
+
+    // const nameToGreet = core.getInput('who-to-greet');
+    // console.log(`Hello ${nameToGreet}!`);
+    // const time = (new Date()).toTimeString();
+    // core.setOutput("time", time);
+    // // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
